a28687dd7a8630c467795fc6df9e48da8b0c9620
zarab <musfiqzarab@iut-dhaka.edu>
2019-03-10 16:04:35 +0600

    error handling

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const URL =
   "https://api.darksky.net/forecast/9a98c19afa3eacb9244789b9a88001ae/23.77,90.399?units=si";
 
 request({ url: URL, json: true }, (error, response) => {
-  const data = response.body.currently;
-  console.log(
-    ` There is ${data.temperature} out there and ${
-      data.precipProbability
-    } % chance of rain`
-  );
+  if (error) {
+    console.log("Unable to connect to weather service ");
+  } else if (response.body.error) {
+    console.log(response.body.error);
+  } else {
+    const data = response.body.currently;
+    console.log(
+      ` There is ${data.temperature} out there and ${
+        data.precipProbability
+      } % chance of rain`
+    );
+  }
 });
 
 //Geocoding
@@ -18,9 +24,15 @@ const URL_geo =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiemFyYWIiLCJhIjoiY2p0Mm5sN2poMjh5dDQ5b2R3ZGx5aWdxcCJ9.KgU8OPkzcylxfbKMcFgEUQ";
 
 request({ url: URL_geo, json: true }, (error, response) => {
-  const data = response.body.features[0];
-  const place = data.place_name;
-  const latitude = data.center[1];
-  const longitude = data.center[0];
-  console.log(` The lan/long for ${place} is ${latitude} and ${longitude}`);
+  if (error) {
+    console.log("Unable to connect to Geo Location service ");
+  } else if (!response.body.features.length) {
+    console.log("Did not find the location");
+  } else {
+    const data = response.body.features[0];
+    const place = data.place_name;
+    const latitude = data.center[1];
+    const longitude = data.center[0];
+    console.log(` The lan/long for ${place} is ${latitude} and ${longitude}`);
+  }
 });
